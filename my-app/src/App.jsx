616047518323a9e1d93c609b7c7168928473540a
abyss-s <xyz@inha.edu>
2024-07-01 02:46:02 +0900

    Refactor: #5 페이지 이름 Login으로 변경 및 Router 연결

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import SignUpFirst from "./pages/SignUpFirst";
 import SignUpSecond from "./pages/SignUpSecond";
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup/1" element={<SignUpFirst />} />
         <Route path="/signup/2" element={<SignUpSecond />} />
       </Routes>
