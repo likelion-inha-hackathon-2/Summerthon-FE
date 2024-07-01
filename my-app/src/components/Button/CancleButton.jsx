376ed33d5e2c29b305edb258f5423de7198fff85
abyss-s <xyz@inha.edu>
2024-07-01 17:01:32 +0900

    Feat: #10 호출 취소하기 버튼 만들기

---
+++ my-app/src/components/Button/CancleButton.jsx
@@ -0,0 +1,16 @@
+// 택시 취소 버튼
+
+import React from "react";
+import Image from "../Image/Image";
+import { ButtonCancle } from "../../assets/image";
+
+const CancleButton = () => {
+  return (
+    <div>
+      {/* 지도 */}
+      <Image src={ButtonCancle}></Image>
+    </div>
+  );
+};
+
+export default CancleButton;
