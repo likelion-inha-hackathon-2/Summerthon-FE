8219ca6def5ba72d5d3917c5e8b43e66060bc8cf
abyss-s <xyz@inha.edu>
2024-07-01 12:49:32 +0900

    Feat: #10 버튼 이미지 업로드 및 Image 컴포넌트 클릭 포커싱 수정

--- my-app/src/assets/image.js
+++ my-app/src/assets/image.js
@@ -3,3 +3,9 @@ export { default as ImageTaxi } from "./images/taxi.png";
 
 // 프로필 용 유저 이미지
 export { default as ImageUser } from "./images/user.png";
+
+// 원형 버튼 이미지
+export { default as ButtonCall } from "./images/button-call.png";
+export { default as ButtonEmer } from "./images/button-emer.png";
+export { default as ButtonScan } from "./images/button-scan.png";
+export { default as ButtonCancle } from "./images/button-cancel.png";
