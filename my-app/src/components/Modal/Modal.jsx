f0c7d37b8dbb0702253e250b87fed9993b44f87d
abyss-s <xyz@inha.edu>
2024-07-02 20:47:42 +0900

    Feat: #5 취소 시 팝업창 Modal 컴포넌트 구현

---
+++ my-app/src/components/Modal/Modal.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+
+const Dialog = ({ isDialog, onClose, onConfirm }) => {
+  return (
+    <StyledDialog isOpen={isDialog}>
+      <DialogContent>
+        정말로 취소하시겠습니까?
+        <ButtonWrapper>
+          <ConfirmBtn onClick={onConfirm}>예</ConfirmBtn>
+          <CloseBtn onClick={onClose}>아니오</CloseBtn>
+        </ButtonWrapper>
+      </DialogContent>
+    </StyledDialog>
+  );
+};
+
+// 'isOpen' 오류 해결
+const StyledDialog = styled.div.attrs(props => ({
+  'aria-hidden': !props.isOpen,
+}))`
+  display: ${(props) => (props.isOpen ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+`;
+
+const DialogContent = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+`;
+
+const CloseBtn = styled.button`
+  padding: 10px;
+  border: none;
+  background-color: #007bff;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const ConfirmBtn = styled.button`
+  padding: 10px;
+  border: none;
+  background-color: red;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: darkred;
+  }
+`;
+
+const Modal = ({ isDialog, onClose, onConfirm }) => {
+  return (
+    <div>
+      <Dialog isDialog={isDialog} onClose={onClose} onConfirm={onConfirm} />
+    </div>
+  );
+};
+
+export default Modal;
