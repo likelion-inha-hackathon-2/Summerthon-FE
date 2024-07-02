d026ffd1910871c70052879e0cec8b835a4025c6
abyss-s <xyz@inha.edu>
2024-07-03 03:28:55 +0900

    Chore: #10 카카오API파일 수정 - 주소 위도 경도 양방향 변환 함수 정의

--- my-app/src/apis/kakaoApi.js
+++ my-app/src/apis/kakaoApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const kakaoApi = axios.create({
-  baseURL: "https://dapi.kakao.com/v2/local/geo/coord2address.json",
+  baseURL: "https://dapi.kakao.com/v2/local",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
@@ -12,15 +12,33 @@ const kakaoApi = axios.create({
 // 주소를 위도/경도로 변환하는 함수
 export const getAddressToCoordinate = async (address) => {
   try {
-    const response = await kakaoApi.get("", {
+    const response = await kakaoApi.get("/search/address.json", {
       params: {
         query: address,
-        input_coord: "WGS84",
       },
     });
+    console.log("getAddressToCoordinate response:", response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching coordinates from address:", error);
+    throw error;
+  }
+};
+
+// 좌표를 주소로 변환하는 함수
+export const getCoordinateToAddress = async (x, y) => {
+  try {
+    const response = await kakaoApi.get("/geo/coord2address.json", {
+      params: {
+        x: x,
+        y: y,
+        input_coord: "WGS84",
+      },
+    });
+    console.log("getCoordinateToAddress response:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching address from coordinates:", error);
     throw error;
   }
 };
