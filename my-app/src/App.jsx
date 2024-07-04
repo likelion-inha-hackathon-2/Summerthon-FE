b1418f73db3a0dddc547d51dce832ad384fa0006
abyss-s <xyz@inha.edu>
2024-07-05 00:34:41 +0900

    Fix: #9 내 정보 페이지 추가 등록 리팩토링

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,10 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"; // 홈(시작)
 import Login from "./pages/Login"; // 로그인
 import SignUp from "./pages/SignUp"; // 회원가입 1
-import Main from "./pages/Main";
+import Main from "./pages/Main"; // 메인 페이지(호출 방식을 선택)
 import Call from "./pages/Call"; // 일반 호출
 import MyInfo from "./pages/MyInfo"; // 내정보
-import ArriveLocation from "./pages/ArriveLocation"; // QR 스캔1
+import MoreMyInfo from "./pages/MoreMyInfo";
+import ArriveLocation from "./pages/ArriveLocation"; // QR 스캔1 (주소지 목록 불러오기)
 import Scan from "./pages/Scan"; // QR 스캔2
 import EmerCall from "./pages/EmerCall"; // 긴급 호출
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/main" element={<Main />} />
         <Route path="/call" element={<Call />} />
         <Route path="/me" element={<MyInfo />} />
+        <Route path="/more-me" element={<MoreMyInfo />} />
         <Route path="/arrive-location" element={<ArriveLocation />} />
         <Route path="/scan" element={<Scan />} />
         <Route path="/emercall" element={<EmerCall />} />
